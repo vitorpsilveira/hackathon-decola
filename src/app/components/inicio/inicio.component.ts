6b8d9fada2fd6b58beda3184c93fd13bc7ddc6f4
Vitor Silveira <silveira_vitor@outlook.com>
2020-07-04 14:54:42 -0300

    Variavel nome, acertos e erros

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardQuizService } from 'src/app/Services/card-quiz.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  nome:string = '';
-
-  constructor() { }
+  constructor(public apiNome: CardQuizService) { }
 
   ngOnInit(): void {
   }
