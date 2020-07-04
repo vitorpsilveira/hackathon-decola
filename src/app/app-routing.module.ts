a912ed0503912a1d3dc655cee87d8b4efb08c092
Lucas Queiroz <lucasqueirozwork@gmail.com>
2020-07-04 15:42:13 -0300

    ranking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SobrePageComponent } from './components/sobre-page/sobre-page.component
 import { QuizPageComponent } from './components/quiz-page/quiz-page.component';
 import { EsportesPageComponent } from './components/esportes-page/esportes-page.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { RankingComponent } from './components/ranking/ranking.component';
 
 
 const routes: Routes = [
@@ -14,7 +15,8 @@ const routes: Routes = [
       { path: '', component: InicioComponent },
       { path: 'quiz', component: QuizPageComponent },
       { path: 'sobre', component: SobrePageComponent },
-      { path: 'esportes', component: EsportesPageComponent }
+      { path: 'esportes', component: EsportesPageComponent },
+      { path: 'ranking', component: RankingComponent }
     ]
   },
 ];
